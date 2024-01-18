d0a603f412b8adb8f72228cfa8e2f612a55ce997
Kye <kye@apacmediasolutions.com>
2024-01-18 12:05:52 -0500

    [CLEANUP][Prototype]

--- gats_torch/main.py
+++ gats_torch/main.py
@@ -1,14 +1,17 @@
 import torch
 from torch import nn, Tensor
-from einops import rearrange, reduce
-from zeta.nn import LocalAttention, Attention
-from classifier_free_guidance_pytorch import (
-    classifier_free_guidance_class_decorator,
+from zeta.nn import (
+    # LocalAttention,
+    img_to_text,
+    video_to_text,
+    audio_to_text,
 )
-from zeta import exists
+from local_attention import LocalAttention
 
 
-@classifier_free_guidance_class_decorator
+
+
+# @classifier_free_guidance_class_decorator
 class GATSBlock(nn.Module):
     def __init__(
         self,
@@ -20,6 +23,7 @@ class GATSBlock(nn.Module):
         causal: bool = True,
         look_backward: int = 1,
         look_forward: int = 0,
+        seqlen: int = 1028,
         *args,
         **kwargs,
     ):
@@ -29,8 +33,9 @@ class GATSBlock(nn.Module):
         self.dim_head = dim_head
         self.dropout = nn.Dropout(dropout)
         self.window_size = window_size
+        self.seqlen = seqlen
 
-        inner_dim = dim_head * heads
+        dim_head * heads
 
         self.local_attn = LocalAttention(
             window_size,
@@ -39,16 +44,47 @@ class GATSBlock(nn.Module):
             look_forward,
             dropout,
             dim=dim,
+            autopad=True,
             *args,
             **kwargs,
         )
-    
 
     def forward(
         self,
         text: Tensor,
         img: Tensor = None,
         audio: Tensor = None,
-        Video: Tensor = None,
+        video: Tensor = None,
+        action: Tensor = None,
+        mask: Tensor = None,
     ):
-        pass
+        img = img_to_text(img, self.seqlen, self.dim, True)
+        audio = audio_to_text(audio, self.seqlen, self.dim, True)
+        video = video_to_text(video, self.seqlen, self.dim, True)
+        
+        x = torch.cat((text, img, audio, video))
+        x = self.local_attn(text, audio, video)
+        
+        print(x.shape)
+        return x
+
+model = GATSBlock(
+    dim=512,
+    heads=8,
+    dim_head=64,
+    dropout=0.1,
+    window_size=512,
+    causal=True,
+    look_backward=1,
+    look_forward=0,
+    seqlen=512 * 2,
+)
+
+text = torch.randn(1, 1024, 512)
+img = torch.randn(1, 3, 224, 224)
+audio = torch.randn(1, 100)
+video = torch.randn(1, 3, 16, 224, 224)
+mask = torch.ones(1, 2057).bool()
+
+out = model(text, img, audio, video, mask=mask)
+print(out)
